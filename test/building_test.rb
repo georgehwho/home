0ab75e64321bf624bfaefcf454765a267762fbcd
George Ho <george.h.w.ho@gmail.com>
2021-02-17 11:35:22 -0500

    Add building test for iteration 2

--- test/building_test.rb
+++ test/building_test.rb
@@ -5,4 +5,33 @@ require './lib/apartment'
 require './lib/renter'
 
 class BuildingTest < Minitest::Test
+  def setup
+    @building = Building.new
+    @unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    @unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+    @renter1 = Renter.new("Aurora")
+    @renter2 = Renter.new("Tim")
+  end
+
+  def test_it_exists
+    assert_instance_of Building, @building
+  end
+
+  def test_it_has_readable_attributes
+    assert_equal [], @building.units
+    assert_equal [], @building.renters
+  end
+
+  def test_it_can_add_units
+    @building.add_unit(@unit1)
+    @building.add_unit(@unit2)
+    assert_equal [@unit1, @unit2], @building.units
+  end
+
+  def test_it_can_add_renters
+    @building.add_unit(@unit1)
+    @building.add_unit(@unit2)
+    @unit1.add_renter(@renter1)
+    assert_equal ['Aurora'], @building.renters
+  end
 end
